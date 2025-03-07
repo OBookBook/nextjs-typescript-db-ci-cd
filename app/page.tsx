39f60a3b5cc07b0eae2efff58bcc9f3f740a77fc
N.I <iworksWeb@gmail.com>
2025-03-07 22:44:21 +0900

    lint test component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { type FC } from "react";
+import HelloWorldLabel from "components/hello-world-label";
 
 const Home: FC = () => {
   return (
@@ -6,6 +7,7 @@ const Home: FC = () => {
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">Hello Wrold</h1>
+          <HelloWorldLabel />
         </div>
       </div>
     </main>
