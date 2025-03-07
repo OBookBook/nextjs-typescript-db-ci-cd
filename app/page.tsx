30fb225000f1ff6b7b1cc0eee08c592d7e319198
N.I <iworksWeb@gmail.com>
2025-03-07 23:44:38 +0900

    npm run format:fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { HelloWorldLabel } from "components/hello-world-label";
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">Hello Wrold</h1>
           <HelloWorldLabel />
